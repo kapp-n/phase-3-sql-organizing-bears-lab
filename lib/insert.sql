7124e9a2c79a15ab4aee5bd86fb05b0c6eb608bd
Mollie Dickenson <molliedickenson@gmail.com>
2021-05-28 09:44:53 -0400

    commit tree 7283c83aae2e045d5dbf33759e05197282d4cd98

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0);
+INSERT INTO bears (2,"Rowdy", 10, "M", "black", "intense", 1);
+INSERT INTO bears (3,"Tabitha", 6, "F", "dark brown", "nice", 1);
+INSERT INTO bears (4,"Sargeant Brown", 19, "M", "Green", "handsome", 0);
+INSERT INTO bears (5,"Melissa", 13, "F", "dark brown", "silly", 1);
+INSERT INTO bears (6,"Grinch", 2, "M", "Black", "grumpy", 1);
+INSERT INTO bears (7,"Wendy", 6, "F", "Blue", "shy", 1);
+INSERT INTO bears (8,null, 20, "M", "black", "aggressive", 0);
